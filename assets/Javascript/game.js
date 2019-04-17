88dfd32868cb7a17b2ff246c6057a92c9f7d77fb
Kellin Baldenegro <kellinjoshua@gmail.com>
2019-04-17 11:26:34 -0700

    final commit

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,45 +1,76 @@
 
 
 //Array of possible answers
-var alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+var guessedLetters = [];
+
+var letterToGuess = null;
+
+var guessLeft = 9;
 
 var wins = 0
 var losses = 0
-var guessLeft = 10
+
+var updateGuessesLeft = function () {
+
+    document.querySelector("#guess-left").innerHTML = guessLeft;
+
+};
+
+var updateLetterToGuess = function () {
+
+    letterToGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+};
+
+var updateGuessesSoFar = function () {
+
+    Document.querySelector("#guesses-so-far").innerHTML = guessedLetters.join(",");
+
+};
+
+var reset = function () {
+    guessesLeft = 9;
+    guessedLetter = [];
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+};
+
+updateLetterToGuess();
+updateGuessesLeft();
 
 
+document.onkeyup = function (event) {
 
-document.onkeyup = function(event) {
+    guessesLeft--;
 
-    var playerGuess = event.key;
+    var alphabet = event.key.toLowerCase();
 
-    var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    
-    //Create a for-loop to iterate through the letters array. Does that even make sense?
+    guessedLetters.push(alphabet);
 
-    userGuess = for (var i = alphabet[0]; i < alphabet.length; i++) {
-        document.write(alphabet[i])
-    }
+    updateGuessesLeft();
+    updateGuessesSoFar();
 
-    if (playerGuess === randomletter) {
-        
+
+    if (alphabet === letterToGuess) {
+
         wins++;
+        document.querySelector("#wins").innerHTML = wins;
+
+        reset();
     }
-    else if (guessLeft === 10) {
-        losses++;  
-    }
-    else{
-       
+    if (guessLeft === 0) {
+
         losses++;
+        document.querySelector("#losses").innerHTML = losses;
+
+        reset();
     }
+};
 
-    //for loop for number of guess left?
 
-    for (var j = 10; j > 0; j--)
-}
-    
-    
 
 
 
